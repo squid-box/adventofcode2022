0389a7cfa5c4a402851a38870a39acd80fc50bcb
Joel Ahlgren <joel@ahlgren.io>
2022-12-01 07:33:22 +0100

    Solved day one.

--- Source/AdventOfCode2022.Tests/Problems/Problem1Tests.cs
+++ Source/AdventOfCode2022.Tests/Problems/Problem1Tests.cs
@@ -6,6 +6,34 @@ namespace AdventOfCode2022.Tests.Problems
     [TestFixture]
     public class Problem1Tests
     {
+        private readonly string[] _testInput = new[]
+        {
+            "1000",
+            "2000",
+            "3000",
+            "",
+            "4000",
+            "",
+            "5000",
+            "6000",
+            "",
+            "7000",
+            "8000",
+            "9000",
+            "",
+            "10000"
+        };
 
+        [Test]
+        public void TestPartOne()
+        {
+            Assert.AreEqual(24000, Problem1.SolvePartOne(_testInput));
+        }
+
+        [Test]
+        public void TestPartTwo()
+        {
+            Assert.AreEqual(45000, Problem1.SolvePartTwo(_testInput));
+        }
     }
 }
--- Source/AdventOfCode2022/Problems/Problem1.cs
+++ Source/AdventOfCode2022/Problems/Problem1.cs
@@ -1,5 +1,9 @@
 namespace AdventOfCode2022.Problems
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using AdventOfCode2022.Utils.Extensions;
+
     /// <summary>
     /// Solution for <a href="https://adventofcode.com/2022/day/1">Day 1</a>.
     /// </summary>
@@ -10,13 +14,47 @@ namespace AdventOfCode2022.Problems
         /// <inheritdoc />
         protected override object SolvePartOne()
         {
-            return "Unsolved";
+            return SolvePartOne(Input);
         }
 
         /// <inheritdoc />
         protected override object SolvePartTwo()
         {
-            return "Unsolved";
+            return SolvePartTwo(Input);
+        }
+
+        private static IEnumerable<int> ParseInput(IEnumerable<string> input)
+        {
+            var elvesWithFood = new List<int>();
+            var currentElf = 0;
+
+            foreach (var foodItem in input)
+            {
+                if (elvesWithFood.Count < currentElf + 1)
+                {
+                    elvesWithFood.Add(0);
+                }
+
+                if (string.IsNullOrEmpty(foodItem))
+                {
+                    currentElf++;
+                    continue;
+                }
+
+                elvesWithFood[currentElf] += foodItem.ToInt();
+            }
+
+            return elvesWithFood;
+        }
+
+        internal static int SolvePartOne(IEnumerable<string> input)
+        {
+            return ParseInput(input).Max();
+        }
+
+        internal static int SolvePartTwo(IEnumerable<string> input)
+        {
+            return ParseInput(input).Order().Reverse().Take(3).Sum();
         }
     }
 }
