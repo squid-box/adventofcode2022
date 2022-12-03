4e339b47f04a4ce83a4b23efd031c56ec990eefe
Joel Ahlgren <joel@ahlgren.io>
2022-12-03 12:01:43 +0100

    Day 3, part one solved.

--- Source/AdventOfCode2022.Tests/Problems/Problem3Tests.cs
+++ Source/AdventOfCode2022.Tests/Problems/Problem3Tests.cs
@@ -6,6 +6,26 @@ namespace AdventOfCode2022.Tests.Problems
     [TestFixture]
     public class Problem3Tests
     {
+        private readonly string[] _testInput =
+        {
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw"
+        };
 
+        [Test]
+        public void TestPartOne()
+        {
+            Assert.AreEqual(157, Problem3.SolvePartOne(_testInput));
+        }
+
+        [Test]
+        public void TestPartTwo()
+        {
+            Assert.AreEqual(70, Problem3.SolvePartTwo(_testInput));
+        }
     }
 }
--- Source/AdventOfCode2022/Problems/Problem3.cs
+++ Source/AdventOfCode2022/Problems/Problem3.cs
@@ -1,5 +1,8 @@
 namespace AdventOfCode2022.Problems
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     /// <summary>
     /// Solution for <a href="https://adventofcode.com/2022/day/3">Day 3</a>.
     /// </summary>
@@ -10,13 +13,69 @@ namespace AdventOfCode2022.Problems
         /// <inheritdoc />
         protected override object SolvePartOne()
         {
-            return "Unsolved";
+            return SolvePartOne(Input);
         }
 
         /// <inheritdoc />
         protected override object SolvePartTwo()
         {
-            return "Unsolved";
+            return SolvePartTwo(Input);
+        }
+
+        internal static IList<(IList<char> first, IList<char> second)> ParseInput(IEnumerable<string> input)
+        {
+            var result = new List<(IList<char>, IList<char>)>();
+
+            foreach (var line in input)
+            {
+                result.Add((line.Take(line.Length / 2).ToList(), line.Skip(line.Length / 2).ToList()));
+            }
+
+            return result;
+        }
+
+        internal static int SolvePartOne(ICollection<string> input)
+        {
+            var score = 0;
+
+            var parsed = ParseInput(input);
+
+            foreach (var pair in parsed)
+            {
+                score += FindScore(FindCommonChar(pair.first, pair.second));
+            }
+
+            return score;
+        }
+
+        internal static char FindCommonChar(IList<char> first, IList<char> second)
+        {
+            for (var i = 0; i < first.Count; i++)
+            {
+                if (second.Contains(first[i]))
+                {
+                    return first[i];
+                }
+            }
+
+            return '\0';
+        }
+
+        internal static int FindScore(char item)
+        {
+            if (char.IsLower(item))
+            {
+                return item - 96;
+            }
+            else
+            {
+                return item - 38;
+            }
+        }
+
+        internal static int SolvePartTwo(ICollection<string> input)
+        {
+            return 0;
         }
     }
 }
