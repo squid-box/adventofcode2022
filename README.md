529bafc02176b72d4f5ff096e896604b07079bb3
Joel Ahlgren <joel.ahlgren@tobiidynavox.com>
2022-12-15 17:14:00 +0100

    WIP

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ The .exe will download the input of the requested day if it's missing.
 | [12](https://adventofcode.com/2022/day/12) | Hill Climbing Algorithm | :star: :star: |
 | [13](https://adventofcode.com/2022/day/13) | Distress Signal         |               |
 | [14](https://adventofcode.com/2022/day/14) | Regolith Reservoir      | :star: :star: |
-| [15](https://adventofcode.com/2022/day/15) | Unknown                 |               |
+| [15](https://adventofcode.com/2022/day/15) | Beacon Exclusion Zone   |               |
 | [16](https://adventofcode.com/2022/day/16) | Unknown                 |               |
 | [17](https://adventofcode.com/2022/day/17) | Unknown                 |               |
 | [18](https://adventofcode.com/2022/day/18) | Unknown                 |               |
--- Source/AdventOfCode2022/Problems/Problem15.cs
+++ Source/AdventOfCode2022/Problems/Problem15.cs
@@ -1,6 +1,9 @@
 namespace AdventOfCode2022.Problems;
 
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using AdventOfCode2022.Utils;
+using AdventOfCode2022.Utils.Extensions;
 
 /// <summary>
 /// Solution for <a href="https://adventofcode.com/2022/day/15">Day 15</a>.
@@ -21,9 +24,36 @@ public class Problem15 : ProblemBase
         return SolvePartTwo(Input);
     }
 
-    internal static long SolvePartOne(ICollection<string> input)
+    private static (HashSet<Coordinate> Sensors, HashSet<Coordinate> Beacons) ParseInput(IEnumerable<string> input)
     {
+        var regex = new Regex(@"Sensor at x=(?<sensorX>-?\d+), y=(?<sensorY>-?\d+): closest beacon is at x=(?<beaconX>-?\d+), y=(?<beaconY>-?\d+)");
+
+        var sensors = new HashSet<Coordinate>();
+        var beacons = new HashSet<Coordinate>();
+
+        foreach (var line in input)
+        {
+            var match = regex.Match(line);
+
+            sensors.Add(new Coordinate(match.Groups["sensorX"].Value.ToInt(), match.Groups["sensorY"].Value.ToInt()));
+            beacons.Add(new Coordinate(match.Groups["beaconX"].Value.ToInt(), match.Groups["beaconY"].Value.ToInt()));
+        }
+
+        return (sensors, beacons);
+    }
+
+    private static int FindImpossibleLocationsOnLevel(int yLevel, HashSet<Coordinate> sensors, HashSet<Coordinate> beacons)
+    {
+
+
         return 0;
+    }
+
+    internal static long SolvePartOne(ICollection<string> input, int yLevel)
+    {
+        var (sensors, beacons) = ParseInput(input);
+
+        return FindImpossibleLocationsOnLevel(yLevel, sensors, beacons);
     }
 
     internal static long SolvePartTwo(ICollection<string> input)
